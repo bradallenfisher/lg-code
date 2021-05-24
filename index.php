e2f529eb153b028a15514501c77bd02a21b2976b
brad <bradallenfisher@gmail.com>
2021-05-24 18:36:17 +0000

    incremental pushs

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <!doctype html>
-
 <html lang="en">
 <head>
   <meta charset="utf-8">
-
-  <title>The HTML5 Herald</title>
-  <meta name="description" content="Fastest LG in the world">
-  <meta name="author" content="brad fisher">
   <link rel="stylesheet" href="css/styles.css">
-</head>
+  	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 
+  <title>SITE NAME</title>
+  <meta name="description" content="big long description goes here"/>
+  <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
+  <link rel="canonical" href="https://lg-code.bludigitalsolutions.com" />
+  <meta property="og:locale" content="en_US">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="PAGE - SITE NAME">
+  <meta property="og:description" content="OPEN GRAPH DESCRIPTION">
+  <meta property="og:url" content="https://lg-code.bludigitalsolutions.com">
+  <meta property="og:site_name" content="SITE NAME">
+  <meta property="og:updated_time" content="2021-05-03T22:22:40+00:00">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="PAGE - SITE NAME">
+  <meta name="twitter:description" content="big long description goes here">
+  <meta name="twitter:site" content="@sitename">
+  <meta name="twitter:creator" content="@sitename">
+</head>
 <body>
 <h1>Hi</h1>
 <?php include_once("inc/menu.php");?>
+
+<!-- hero -->
+<div class="header">
+  <h1>Header</h1>
+  <p>My supercool header</p>
+</div>
+<!-- end hero -->
+
 </body>
 </html>

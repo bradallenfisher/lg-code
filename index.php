1f216ddbc660637e8fdc99a88841529f1244eded
brad <bradallenfisher@gmail.com>
2021-05-24 17:24:12 +0000

    hithere php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="Fastest LG in the world">
+  <meta name="author" content="brad fisher">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+<h1>Hi</h1>
+<?php echo "hi there";?>
+</body>
+</html>
